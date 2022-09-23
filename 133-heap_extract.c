52f21438096468207457054c63188c23c7b1090f
bazboy36 <shokunbiabdulbasit1@gmail.com>
2022-09-22 17:47:26 -0700

    heap extract

--- 133-heap_extract.c
+++ 133-heap_extract.c
@@ -1,174 +1,79 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - Insert a new node in the tree.
- *
- * @parent: Parent node.
- * @value: Value of the node.
- *
- * Return: Always 0 (Success)
+ * max - Finds the maximum node in a tree.
+ * @tree: The pointer to the root of the tree.
+ * Return: The node with the maximum value.
  */
-
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+heap_t *max(heap_t *tree)
 {
-	binary_tree_t *new_node;
+	heap_t *curr_max, *left_max, *right_max;
 
-	new_node = malloc(sizeof(binary_tree_t));
-	if (new_node == NULL)
-		return (NULL);
-	new_node->n = value;
-	new_node->left = NULL;
-	new_node->right = NULL;
-	new_node->parent = parent;
-	return (new_node);
+	if (!tree->left)
+		return (tree);
+	left_max = max(tree->left);
+	if (left_max->n > tree->n)
+		curr_max = left_max;
+	else
+		curr_max = tree;
+	if (tree->right)
+	{
+		right_max = max(tree->right);
+		if (right_max->n > curr_max->n)
+			curr_max = right_max;
+		else
+			curr_max = tree;
+	}
+	return (curr_max);
 }
 
 /**
- * check_new_root - checks the new root
- * @root: root
- * @new_root: new root
- * Return: node
+ * recurse_extract - Recursively extracts the max from the tree.
+ * @tree: The pointer to the root of the tree.
  */
-heap_t *check_new_root(heap_t **root, heap_t *new_root)
+void recurse_extract(heap_t *tree)
 {
-	heap_t *head, *current;
+	heap_t *sub_max, *right_max = NULL;
 
-	current = *root;
-	while (1)
+	if (!tree->left)
+		return;
+	sub_max = max((tree)->left);
+	if (tree->right)
+		right_max = max(tree->right);
+	if (right_max && right_max->n > sub_max->n)
+		sub_max = right_max;
+	tree->n = sub_max->n;
+	if (!sub_max->left)
 	{
-		if (binary_tree_balance(current))
-			current = current->left;
-		else if (!binary_tree_balance(current) && current->right)
-			current = current->right;
-		else if (!binary_tree_balance(current))
-			break;
-	}
-	new_root = current, head = *root;
-	if (new_root == head)
-		free(head), new_root = NULL;
-	else if (new_root->parent == head)
-	{
-		if (head->right == new_root)
-		{
-			new_root->left = head->left, head->left->parent = new_root;
-			*root = new_root, free(head);
-		}
-		else
-		{
-			*root = new_root, free(head);
-			new_root->parent = NULL;
-			return (NULL);
-		}
+		if (sub_max->parent && sub_max->parent->left == sub_max)
+			sub_max->parent->left = NULL;
+		if (sub_max->parent && sub_max->parent->right == sub_max)
+			sub_max->parent->right = NULL;
+		free(sub_max);
 	}
 	else
-	{
-		new_root->left = head->left, new_root->left->parent = new_root;
-		new_root->right = head->right, new_root->right->parent = new_root;
-		if (new_root->parent->right == new_root)
-			new_root->parent->right = NULL;
-		else
-			new_root->parent->left = NULL;
-		*root = new_root, free(head);
-	}
-	return (new_root);
-}
-/**
- * if_check - if statement
- * @new_root: new root
- * @tmp: position
- * @current: position
- * Return: void
- */
-void if_check(heap_t *new_root, heap_t *tmp, heap_t *current)
-{
-	if (new_root->right == tmp)
-	{
-		current = tmp->left, tmp->left = new_root->left;
-		if (tmp->left)
-			tmp->left->parent = tmp;
-		new_root->left = current;
-		if (new_root->left)
-			new_root->left->parent = new_root;
-		current = tmp->right, tmp->right = new_root;
-		new_root->right = current;
-	}
-	else
-	{
-		current = tmp->right, tmp->right = new_root->right;
-		if (tmp->right)
-			tmp->right->parent = tmp;
-		new_root->right = current;
-		if (new_root->right)
-			new_root->right->parent = new_root;
-		current = tmp->left, tmp->left = new_root;
-		new_root->left = current;
-	}
+		recurse_extract(sub_max);
 }
 
 /**
- * loop_heap - loop
- * @root: root
- * @new_root: new root
- *
- * Return: void
- */
-void loop_heap(heap_t **root, heap_t *new_root)
-{
-	heap_t *tmp, *current = NULL;
-
-	while (new_root && (new_root->right || new_root->left))
-	{
-		if (new_root->right && new_root->left)
-		{
-			if (new_root->right->n > new_root->left->n)
-				tmp = new_root->right;
-			else
-				tmp = new_root->left;
-		}
-		else if (new_root->left)
-			tmp = new_root->left;
-		else
-			tmp = new_root->right;
-		if (tmp->n > new_root->n)
-		{
-			if_check(new_root, tmp, current);
-		if (current)
-			current->parent = new_root;
-
-		tmp->parent = new_root->parent;
-		if (new_root->parent && new_root->parent->right == new_root)
-			new_root->parent->right = tmp;
-		else if (new_root->parent)
-			new_root->parent->left = tmp;
-		new_root->parent = tmp;
-		if (tmp && !tmp->parent)
-			*root = tmp;
-		}
-		else
-			break;
-	}
-}
-
-/**
- * heap_extract - removes head
- * @root: root
- *
- * Return: value in root
+ * heap_extract - Extracts the root from a Binary Heap.
+ * @root: The pointer to the root of the tree.
+ * Return: The value of the extracted node.
  */
 int heap_extract(heap_t **root)
 {
-	heap_t *new_root = NULL;
 	int value;
 
 	if (!*root)
 		return (0);
-
 	value = (*root)->n;
-	new_root = check_new_root(root, new_root);
-	if (!new_root)
+	if (!(*root)->left)
+	{
+		value = (*root)->n;
+		free(*root);
+		*root = NULL;
 		return (value);
-	new_root->parent = NULL;
-
-	loop_heap(root, new_root);
+	}
+	recurse_extract(*root);
 	return (value);
 }
